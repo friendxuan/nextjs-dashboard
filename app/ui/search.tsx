1c3f1858a35e1edfb279e31e396792ba53de28cf
friendxuan <friendxuan@163.com>
2025-06-03 21:11:50 +0800

    Chapter 10 部分预渲染 Chapter 11 搜索和分页 Chapter 12 更改数据

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce"; // 防抖
+/**
+  useDebouncedCallback(callback, delay, options?)
+  callback: 要被防抖处理的函数（在这里就是更新 URL 的逻辑）。
+  delay: 延迟时间（单位毫秒），这里是 300。
+  options?: 可选配置项（比如是否在首次点击前也触发一次等）。
+ */
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1"); // 重置页码为 1
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
