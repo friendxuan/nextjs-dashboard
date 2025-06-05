d05d29dff95fb4b69dc261701386deae0606f772
friendxuan <friendxuan@163.com>
2025-06-06 01:03:17 +0800

    Chapter 16 添加元数据

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'; // 全局样式 Tailwind
 import { inter } from '@/app/ui/fonts'; // 添加主字体
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
  
 export default function RootLayout({
   children,
